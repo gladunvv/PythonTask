449f116a1ce7310b66f14432625fbba265731ac3
Vladislav Gladun <bincha.1997@gmail.com>
2018-10-26 19:27:46 +0300

    Update

--- taskPyth.py
+++ taskPyth.py
@@ -10,8 +10,9 @@ print(s[::2])
 # Вшитие переменных в текст
 age = 10
 name = 'Lisa'
+print("Hello my cat's name is {a} and she is {b} years old!".format(a = name, b = age))
 print('Hello my cat\'s name is ' + name +  ' and she is ' + str(age) + ' years old!')
-# Замена значение в списке
+# Замена значения в списке
 l = [21, 2, [3, 12, True], [10, 13, 'Hello', False]]
 l[3][2] = 'Goodbay'
 print(l)
