b2400dc905dad37c996c4db1bc319f28a13653c1
Vladislav Gladun <bincha.1997@gmail.com>
2018-10-29 16:23:15 +0300

    Learn Python on Stepic

---
+++ StepicTask.py
@@ -0,0 +1,18 @@
+# Будильник
+# X = int(input())
+# H = int(input())
+# M = int(input())
+# H *=60
+# print((X + H + M)//60)
+# print((X + H + M)%60) 
+
+# Приоритет логических операций
+# ((a and b) or ((not a) and (not b))))
+
+#Логические операции
+# x = 5
+# y = 10
+# a = True
+# b = False
+# print(a and b or not a and not b)
+# print(y > x * x or y >= 2 * x and x < y)
