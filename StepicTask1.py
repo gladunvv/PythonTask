9b7f85d7d0b0591b7f9d9c1917ce6472849552fe
Vladislav Gladun <bincha.1997@gmail.com>
2018-10-30 12:57:24 +0300

    Learn Python on Stepic N2

---
+++ StepicTask1.py
@@ -0,0 +1,28 @@
+# Режим сна
+# A = int(input())
+# B = int(input())
+# H = int(input())
+# if H < A:
+#     print("Недосып")
+# elif H > B:
+#     print("Пересып")
+# else:
+#     print("Это нормально")
+
+# Високосный ли год
+# A = int(input())
+# if A % 4 == 0 and A % 100 != 0:
+#     print("Високосный")
+# elif A % 400 == 0:
+#     print("Високосный")
+# else: 
+#     print("Обычный")
+
+# Вычисление площади треугольника. Формула Герона
+
+# a = (int(input()))
+# b = (int(input()))
+# c = (int(input()))
+# p = ((a + b + c)/2)
+# s = float((p*(p-a)*(p-b)*(p-c))**0.5)
+# print(s)
